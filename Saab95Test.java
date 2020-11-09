14b601d15040fa28fa31048ee8357b4c5ee8b4a8
Staol <kolontayev1@gmail.com>
2020-11-09 18:21:24 +0100

    Create Saab95Test and added 3 test methods

---
+++ Saab95Test.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Saab95Test {
+
+    @Test
+    void setTurboOn() {
+        Saab95 saab95 = new Saab95();
+        saab95.setTurboOn();
+        assertTrue(saab95.isTurboOn());
+    }
+
+    @Test
+    void setTurboOff() {
+        Saab95 saab95 = new Saab95();
+        saab95.setTurboOff();
+        assertFalse(saab95.isTurboOn());
+    }
+
+    @Test
+    void speedFactorTurboOff() {
+        Saab95 saab95 = new Saab95();
+        saab95.setTurboOff();
+        assertEquals(1.25, saab95.speedFactor(), 0.001);
+    }
+
+    @Test
+    void speedFactorTurboOn() {
+        Saab95 saab95 = new Saab95();
+        saab95.setTurboOn();
+        assertEquals(1.625, saab95.speedFactor(), 0.001);
+    }
+
+}
