18e7cfae2a6ae602d2740c3723b700d10f6ad89f
Staol <kolontayev1@gmail.com>
2020-11-09 18:23:58 +0100

    Created Volvo240Test and added 1 test method

---
+++ Volvo240Test.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Volvo240Test {
+
+    @Test
+    void speedFactor(){
+        Volvo240 volvo240 = new Volvo240();
+        assertEquals(1.25, volvo240.speedFactor(), 0.001);
+    }
+
+}
