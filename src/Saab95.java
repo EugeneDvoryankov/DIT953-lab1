552c1149ca3bbf611d7d9a60d7334c222b9240c9
Staol <kolontayev1@gmail.com>
2020-11-10 14:28:56 +0100

    Added javadoc comments

--- src/Saab95.java
+++ src/Saab95.java
@@ -2,10 +2,18 @@ package src;
 
 import java.awt.*;
 
+/**
+ * Represents a Saab95 car
+ */
 public class Saab95 extends Car{
 
     private boolean turboOn;
-    
+
+    /**
+     * Creates a Saab.
+     * with specified nrDoors, color, enginePower, set turboOn to false
+     * specifies modelName and makes a call to stopEngine method.
+     */
     public Saab95(){
         setNrDoors(2);
         setColor(Color.red);
@@ -15,18 +23,30 @@ public class Saab95 extends Car{
         stopEngine();
     }
 
+    /**
+     * Sets turboOn true.
+     */
     public void setTurboOn(){
 	    turboOn = true;
     }
 
+    /**
+     *  Sets turboOn false.
+     */
     public void setTurboOff(){
 	    turboOn = false;
     }
 
+    /**
+     * @return a boolean representing turboOn is true or false.
+     */
     public boolean isTurboOn() {
         return turboOn;
     }
 
+    /**
+     * @return a double representing speedFactor
+     */
     @Override
     public double speedFactor(){
         double turbo = 1;
