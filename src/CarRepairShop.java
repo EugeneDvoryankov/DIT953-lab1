2d931b999ca3eaf95188abbe25b3336532fcfe16
Staol <kolontayev1@gmail.com>
2020-11-18 13:49:59 +0100

    Added CarRepairShop

---
+++ src/CarRepairShop.java
@@ -0,0 +1,48 @@
+import java.util.LinkedList;
+
+/** Represents a Car Repair Shop.
+ *
+ * A Car Repair Shop loads a number of cars, up to a maximum number that can vary between different workshops.
+ * Some workshops should only be able to accommodate a certain type of car;
+ * others can receive any car.
+ * Trying to receive the "wrong" sort of car in a workshop should give a static (compile time) error.
+ * When retrieving a car from the workshop, we must be able to get as specific static type information as possible.
+ * Example: A car retrieved from a branded workshop that only handles the Volvo 240 should have static type Volvo240.
+ * @param <C> a class that extends the Car, either the Volvo240 or Saab95 class
+ */
+public class CarRepairShop<C extends Car> {
+    private LinkedList<C> cars;
+
+    /** Adds a car to the Car Repair Shop
+     *
+     * Cars can only be loaded if: the carTransporter is stationary
+     * ramp is down, and the cars are reasonably close to car
+     * @param item a Car
+     */
+    public void loadCar(C item){
+        cars.push(item);
+    }
+
+    /** Removes a car from the Car Repair Shop
+     *
+     * Cars can only be removed if: the carTransporter is stationary
+     * ramp is down, and the cars are reasonably close to car
+     * @param item a car
+     */
+    public void removeCar(C item){
+        cars.pop();
+    }
+
+    public static void main(String[] args) {
+        Saab95 saab95 = new Saab95();
+        Volvo240 volvo240 = new Volvo240();
+        CarRepairShop<Saab95> saab95RepairShop = new CarRepairShop<>();
+
+        saab95RepairShop.loadCar(saab95);
+        //saab95RepairShop.loadCar(volvo240);
+
+        CarRepairShop<Car> anyCarRepairShop = new CarRepairShop<>();
+        anyCarRepairShop.loadCar(saab95);
+        anyCarRepairShop.loadCar(volvo240);
+    }
+}
